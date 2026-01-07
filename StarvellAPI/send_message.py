eff45a9d5ca0aac5c37dd1c9547995271abab3a9
ну типо вайб <vibeslocaly@gmail.com>
2026-01-07 13:31:21 +0700

    Обновление - добавил watermark.
    Обновление - добавил watermark.
    Обновил - bot.py и send_message.py
    version: 1.0.3

--- StarvellAPI/send_message.py
+++ StarvellAPI/send_message.py
@@ -1,10 +1,28 @@
 import json
 import aiohttp
 
+def load_settings() -> dict:
+    try:
+        with open("config/settings.json", "r", encoding="utf-8") as f:
+            return json.load(f)
+    except (json.JSONDecodeError, IOError, FileNotFoundError):
+        return {}
+
+def get_settings() -> dict:
+    return load_settings()
+
 async def send_chat_message(session_cookie: str, chat_id: str, content: str, chat_name: str=None) -> dict:
+    settings = get_settings()
+    global_switches = settings.get("global_switches", {})
+
+    watermark_text_text = global_switches.get("watermark", "[ Не указан ;/ ]")
+    watermark_enabled = global_switches.get("watermark_enabled", True)
+
+    content_watermark = (watermark_text_text + "\n\n" + content) if watermark_enabled else content
+
     headers = {'accept': '*/*', 'accept-language': 'ru,en;q=0.9', 'content-type': 'application/json', 'origin': 'https://starvell.com', 'referer': f'https://starvell.com/chat/{chat_id}', 'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 YaBrowser/25.8.0.0 Safari/537.36'}
     cookies = {'session': session_cookie, 'starvell.theme': 'dark', 'starvell.time_zone': 'Europe/Moscow', 'starvell.my_games': '10,1,11'}
-    payload = {'chatId': chat_id, 'content': content}
+    payload = {'chatId': chat_id, 'content': content_watermark}
     url = 'https://starvell.com/api/messages/send'
     timeout = aiohttp.ClientTimeout(total=20)
     async with aiohttp.ClientSession(headers=headers, cookies=cookies, timeout=timeout) as session:
